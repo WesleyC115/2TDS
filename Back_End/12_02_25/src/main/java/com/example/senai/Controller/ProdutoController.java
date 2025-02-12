92b248c04ee6dee2f7a35812cabcb0100140e727
WesleyC115 <wesley.moura03@hotmail.com>
2025-02-12 17:30:02 -0300

    commit 2

--- Back_End/12_02_25/src/main/java/com/example/senai/Controller/ProdutoController.java
+++ Back_End/12_02_25/src/main/java/com/example/senai/Controller/ProdutoController.java
@@ -1,2 +1,20 @@
-package com.example.senai.Controller;public class ProdutoController {
+package com.example.senai.Controller;
+
+
+import com.example.senai.DataBase.BancoDeProdutos;
+import com.example.senai.Model.Produto;
+
+import java.util.List;
+
+public class ProdutoController {
+
+    BancoDeProdutos bc = new BancoDeProdutos();
+
+    public void inserirNoBanco(Produto p) {
+        bc.insert(p);
+    }
+
+    public List<Produto> pegarTodosOsProdutos() {
+        return bc.findAll();
+    }
 }
--- Back_End/12_02_25/src/main/java/com/example/senai/View/TesteProduto.java
+++ Back_End/12_02_25/src/main/java/com/example/senai/View/TesteProduto.java
@@ -2,8 +2,12 @@ package com.example.senai.View;
 
 
 import com.example.senai.Model.Produto;
+import com.example.senai.Controller.ProdutoController;
 import org.springframework.web.bind.annotation.*;
-
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/produto")
 public class TesteProduto {
